cad4437b088df0ea84b301788fdf85ee6954f37b
yb <yb@itcast.cn>
2021-01-06 17:07:20 +0800

    添加gittest工程

---
+++ src/com/yidu/t286/FirstClass.java
@@ -0,0 +1,18 @@
+package com.yidu.t286;
+
+/**
+ * 功能：
+ * 作者：燕兵
+ * 时间：2021/1/6 16:57
+ * 版本：1.0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+    }
+}
