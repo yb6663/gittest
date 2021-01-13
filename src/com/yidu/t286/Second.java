0e55d389dd17e2be75aa64924c08283c06cf17b2
yb <yb@itcast.cn>
2021-01-13 15:06:16 +0800

    添加一个Second类

---
+++ src/com/yidu/t286/Second.java
@@ -0,0 +1,13 @@
+package com.yidu.t286;
+
+/**
+ * 功能：
+ * 作者：燕兵
+ * 时间：2021/1/13 15:04
+ * 版本：1.0
+ */
+public class Second {
+    public void test(){
+        System.out.println("hello second!");
+    }
+}
